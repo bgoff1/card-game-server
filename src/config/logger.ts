02a9d85acd9f5add4232315720738a278812179f
Judah Goff <bgoff1@harding.edu>
2020-01-22 23:09:34 -0600

    Add config for relative imports, add logger tests

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,33 +1,42 @@
 import winston from 'winston';
-const logger = winston.createLogger({
-  level: 'debug',
-  format: winston.format.colorize(),
-  transports: [
-    new winston.transports.Console({ format: winston.format.simple() })
-  ]
-});
+export class Logger {
+  logger: winston.Logger;
+  constructor() {
+    this.logger = winston.createLogger({
+      level: 'debug',
+      format: winston.format.colorize(),
+      transports: [
+        new winston.transports.Console({ format: winston.format.simple() })
+      ]
+    });
+  }
 
-// Function to allow format anything to be printable,
-// Logger only allows strings, so this formats it to a printable string
-function parseMessage(message: any): string {
-  switch (typeof message) {
-    case 'string':
-      return message;
-    case 'object':
-      return JSON.stringify(message);
-    case 'number':
-    case 'bigint':
-      return message.toString();
-    case 'boolean':
-      return message ? 'true' : 'false';
-    default:
-      return '';
+  // Function to allow format anything to be printable,
+  // Logger only allows strings, so this formats it to a printable string
+  parseMessage(message: any): string {
+    switch (typeof message) {
+      case 'object':
+        return JSON.stringify(message);
+      case 'number':
+        return message.toString();
+      case 'boolean':
+        return message ? 'true' : 'false';
+    }
+    return message;
+  }
+
+  info(msg: any) {
+    this.logger.info(this.parseMessage(msg));
+  }
+  error(msg: any) {
+    this.logger.error(this.parseMessage(msg));
+  }
+  debug(msg: any) {
+    this.logger.debug(this.parseMessage(msg));
+  }
+  warn(msg: any) {
+    this.logger.info(this.parseMessage(msg));
   }
 }
 
-export default {
-  info: (msg: any) => logger.info(parseMessage(msg)),
-  error: (msg: any) => logger.error(parseMessage(msg)),
-  debug: (msg: any) => logger.debug(parseMessage(msg)),
-  warn: (msg: any) => logger.warn(parseMessage(msg))
-};
+export default new Logger();
